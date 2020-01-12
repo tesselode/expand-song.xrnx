0bbb8bd741006f493c438309b29a168df3bb1a6c
Andrew Minnich <aminnich3@gmail.com>
2020-01-12 02:03:45 -0500

    note expansion

---
+++ main.lua
@@ -0,0 +1,51 @@
+local function to_time(line, delay)
+	return (line - 1) * 256 + delay
+end
+
+local function from_time(time)
+	local line = math.floor(time / 256) + 1
+	local delay = time % 256
+	return line, delay
+end
+
+local function expand_pattern(pattern_index, factor)
+	local song = renoise.song()
+	local pattern = song.patterns[pattern_index]
+	-- get all the note events in this pattern and clear the columns
+	local notes = {}
+	for position, column in song.pattern_iterator:note_columns_in_pattern(pattern_index) do
+		if not column.is_empty then
+			table.insert(notes, {
+				track = position.track,
+				column = position.column,
+				time = to_time(position.line, column.delay_value),
+				note_value = column.note_value,
+				instrument_value = column.instrument_value,
+				volume_value = column.volume_value,
+				panning_value = column.panning_value,
+				effect_number_value = column.effect_number_value,
+				effect_amount_value = column.effect_amount_value,
+			})
+			column:clear()
+		end
+	end
+	-- increase the pattern length
+	pattern.number_of_lines = math.min(pattern.number_of_lines * factor, renoise.Pattern.MAX_NUMBER_OF_LINES)
+	-- write the notes
+	for _, note in ipairs(notes) do
+		note.time = note.time * factor
+		local line, delay = from_time(note.time)
+		if line <= renoise.Pattern.MAX_NUMBER_OF_LINES then
+			local column = pattern.tracks[note.track].lines[line].note_columns[note.column]
+			column.note_value = note.note_value
+			column.instrument_value = note.instrument_value
+			column.volume_value = note.volume_value
+			column.panning_value = note.panning_value
+			column.delay_value = delay
+			column.effect_number_value = note.effect_number_value
+			column.effect_amount_value = note.effect_amount_value
+		end
+	end
+end
+
+expand_pattern(1, 2)
