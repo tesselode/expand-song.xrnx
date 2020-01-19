e04a59452b905580987d13c5f98e6dc0e98a7036
Andrew Minnich <aminnich3@gmail.com>
2020-01-18 20:44:43 -0500

    proper undo description

--- gui.lua
+++ gui.lua
@@ -130,6 +130,7 @@ function gui.show_dialog()
 				width = '100%',
 				height = renoise.ViewBuilder.DEFAULT_DIALOG_BUTTON_HEIGHT,
 				notifier = function()
+					renoise.song():describe_undo 'Expand song'
 					expand.expand_all_patterns(factor)
 					if should_adjust_beat_sync then expand.adjust_beat_sync(factor) end
 					if should_adjust_lpb then expand.adjust_lpb(factor) end
